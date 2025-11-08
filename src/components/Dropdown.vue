a4388b3a3819fac193028e145db4bed80f38083f
glitchykid <glitchykid.next@icloud.com>
2025-11-09 01:41:30 +0300

    chore: maintenance checkpoint

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -34,32 +34,20 @@ const changeValue = (value: string): void => {
   <div class="relative z-50">
     <div
       class="flex flex-row p-4 gap-4 border-1 border-[#C9D7ED] rounded-[8px] min-w-38 justify-between items-center transition-all duration-300"
-      :class="
-        isOpen
-          ? 'bg-[#8276FF] text-[#EAEDF2]'
-          : 'text-[#8276FF] hover:bg-[#D0CCFF]'
-      "
-      @click="toogleDropdown"
-    >
+      :class="isOpen
+        ? 'bg-[#8276FF] text-[#EAEDF2]'
+        : 'text-[#8276FF] hover:bg-[#D0CCFF]'
+        " @click="toogleDropdown">
       <span class="select-none">{{ selected }}</span>
       <AppIcon name="chevronDown" />
     </div>
-    <div
-      v-if="isOpen"
-      class="flex flex-col absolute border-1 bg-white border-[#C9D7ED] w-full top-full rounded-[8px] max-h-60 overflow-auto"
-    >
-      <span
-        v-for="value of values"
-        :key="value"
-        class="text-center gap-y-4 w-full py-2 transition-all duration-300 select-none"
-        :class="
-          selected === value
-            ? 'bg-[#8276FF] text-[#EAEDF2]'
-            : 'text-[#8276FF] hover:bg-[#D0CCFF]'
-        "
-        @click.stop="changeValue(value)"
-        >{{ value }}</span
-      >
+    <div v-if="isOpen"
+      class="flex flex-col absolute border bg-white border-[#C9D7ED] w-full top-full rounded-[8px] max-h-60 overflow-auto">
+      <span v-for="value of values" :key="value"
+        class="text-center gap-y-4 w-full py-2 transition-all duration-300 select-none" :class="selected === value
+          ? 'bg-[#8276FF] text-[#EAEDF2]'
+          : 'text-[#8276FF] hover:bg-[#D0CCFF]'
+          " @click.stop="changeValue(value)">{{ value }}</span>
     </div>
   </div>
 </template>
